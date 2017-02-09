a9db29a4ce09401f2b3a03d42e30309b44875774
Henrik Larsson <laselase@gmail.com>
2017-02-10 00:13:14 +0100

    test connection

--- serialcomputer/serialcomputer.py
+++ serialcomputer/serialcomputer.py
@@ -42,7 +42,7 @@ class SerialComputer:
 
             if self.compSerial.is_open:
                 logging.debug("TestConnection 3")
-                self.compSerial.write(bytearray([0x41])) #noOfBytes =
+                noOfBytes = self.compSerial.in_waiting()
                 logging.debug("TestConnection 4") # + str(noOfBytes)
             else:
                 return False
