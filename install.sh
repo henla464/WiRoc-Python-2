343113b67204dd4d4228b0d1d950d8a470542f35
Henrik Larsson <laselase@gmail.com>
2017-04-20 19:04:45 +0200

    update install

--- install.sh
+++ install.sh
@@ -3,43 +3,43 @@
 
 
 echo "update"
-read line
+#read line
 # update app list
 apt-get update
 
 echo "sqlite3"
-read line
+#read line
 #Install sqlite3
 apt-get install libsqlite3-dev
 
 echo "python/pip"
-read line
+#read line
 #Install python/pip
 apt-get install python3
 apt-get install python3-pip
 
 echo "flask"
-read line
+#read line
 #Install flask
 pip3 install flask
 
 echo "pyserial"
-read line
+#read line
 #Install serial
 pip3 install pyserial
 
 echo "jsonpickle"
-read line
+#read line
 #Install jsonpickle
 pip3 install jsonpickle
 
 echo "pyudev"
-read line
+#read line
 #Install pyudev
 pip3 install pyudev
 
 echo "newer nodejs"
-read line
+#read line
 #Install newer nodejs
 wget https://nodejs.org/dist/v6.9.1/node-v6.9.1-linux-armv7l.tar.xz
 tar -C /usr/local --strip-components 1 -xJf node-v6.9.1-linux-armv7l.tar.xz
@@ -47,30 +47,30 @@ ln -s /usr/local/bin/node /usr/bin/nodejs
 ln -s /usr/local/bin/npm /usr/bin/npm
 
 echo "Install python 2"
-read line
+#read line
 #Install python 2
 apt-get install python
 
 echo "Install bluetooth stuff"
-read line
+#read line
 #Install bluetooth stuff
 apt-get install bluetooth bluez libbluetooth-dev libudev-dev
 
 echo "WiRoc-Python-2"
-read line
+#read line
 #install Python-2
-wget -O WiRoc-Python-2.tar.gz https://github.com/henla464/WiRoc-Python-2/archive/v0.19.tar.gz
+wget -O WiRoc-Python-2.tar.gz https://github.com/henla464/WiRoc-Python-2/archive/v0.20.tar.gz
 rm -rf WiRoc-Python-2
-tar xvfz WiRoc-Python-2.tar.gz WiRoc-Python-2-0.19
-mv WiRoc-Python-2-0.19 WiRoc-Python-2
+tar xvfz WiRoc-Python-2.tar.gz WiRoc-Python-2-0.20
+mv WiRoc-Python-2-0.20 WiRoc-Python-2
 
 echo "WiRoc-BLE"
-read line
+#read line
 #install WiRoc-BLE
-wget -O WiRoc-BLE-Device.tar.gz https://github.com/henla464/WiRoc-BLE-Device/archive/v0.03.tar.gz
+wget -O WiRoc-BLE-Device.tar.gz https://github.com/henla464/WiRoc-BLE-Device/archive/v0.04.tar.gz
 rm -rf WiRoc-BLE-Device
-tar xvfz WiRoc-BLE-Device.tar.gz WiRoc-BLE-Device-0.03
-mv WiRoc-BLE-Device-0.03 WiRoc-BLE-Device
+tar xvfz WiRoc-BLE-Device.tar.gz WiRoc-BLE-Device-0.04
+mv WiRoc-BLE-Device-0.04 WiRoc-BLE-Device
 
 #npm install -g node-gyp
 echo "install bluetooth-hci-socket"
@@ -78,7 +78,7 @@ cd /home/chip/WiRoc-BLE-Device/
 npm install bluetooth-hci-socket
 
 echo "install bleno"
-read line
+#read line
 #install bleno
 
 npm install bleno
@@ -86,7 +86,7 @@ cd /home/chip
 
 
 echo "install startup scripts"
-read line
+#read line
 #Startup scripts:
 mkdir WiRoc-StartupScripts
 wget -O /home/chip/WiRoc-StartupScripts/Startup.sh https://raw.githubusercontent.com/henla464/WiRoc-StartupScripts/master/Startup.sh
@@ -106,14 +106,14 @@ wget -O /etc/udev/rules.d/99-ttyexcludemm.rules https://raw.githubusercontent.co
 udevadm control --reload-rules
 
 echo "install new dtb"
-read line
+#read line
 #Install the new dtb on chip
 rm /boot/sun5i-r8-chip.dtb
 wget -O /boot/sun5i-r8-chip.dtb https://raw.githubusercontent.com/henla464/WiRoc-StartupScripts/master/sun5i-r8-chip.dtb
 
 
 echo "install wiroc-monitor"
-read line
+#read line
 #Install WiRoc-Monitor
 wget -O /usr/local/bin/gpio.sh https://raw.githubusercontent.com/henla464/WiRoc-Monitor/master/gpio.sh
 wget -O /usr/local/bin/blink.sh https://raw.githubusercontent.com/henla464/WiRoc-Monitor/master/blink.sh
@@ -123,7 +123,7 @@ wget -O /usr/local/etc/blink.cfg https://raw.githubusercontent.com/henla464/WiRo
 systemctl enable /etc/systemd/system/blink.service
 
 echo "add user to dialout"
-read line
+#read line
 sudo usermod -a -G dialout $USER
 
 
