c7de53be114915363eb8af73fea54518d908b606
Henrik Larsson <laselase@gmail.com>
2017-05-06 16:32:08 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -59,10 +59,10 @@ apt-get install bluetooth bluez libbluetooth-dev libudev-dev
 echo "WiRoc-Python-2"
 #read line
 #install Python-2
-wget -O WiRoc-Python-2.tar.gz https://github.com/henla464/WiRoc-Python-2/archive/v0.25.tar.gz
+wget -O WiRoc-Python-2.tar.gz https://github.com/henla464/WiRoc-Python-2/archive/v0.29.tar.gz
 rm -rf WiRoc-Python-2
-tar xvfz WiRoc-Python-2.tar.gz WiRoc-Python-2-0.25
-mv WiRoc-Python-2-0.25 WiRoc-Python-2
+tar xvfz WiRoc-Python-2.tar.gz WiRoc-Python-2-0.29
+mv WiRoc-Python-2-0.29 WiRoc-Python-2
 
 echo "WiRoc-BLE"
 #read line
@@ -80,7 +80,6 @@ npm install bluetooth-hci-socket
 echo "install bleno"
 #read line
 #install bleno
-
 npm install bleno
 cd /home/chip
 
