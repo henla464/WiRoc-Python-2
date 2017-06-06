a466104b937393c5c7ab6cb2f53498ee2c5e7fa7
Henrik Larsson <laselase@gmail.com>
2017-06-07 00:37:31 +0200

    install.sh

--- install.sh
+++ install.sh
@@ -59,10 +59,10 @@ apt-get install bluetooth bluez libbluetooth-dev libudev-dev
 echo "WiRoc-Python-2"
 #read line
 #install Python-2
-wget -O WiRoc-Python-2.tar.gz https://github.com/henla464/WiRoc-Python-2/archive/v0.35.tar.gz
+wget -O WiRoc-Python-2.tar.gz https://github.com/henla464/WiRoc-Python-2/archive/v0.36.tar.gz
 rm -rf WiRoc-Python-2
-tar xvfz WiRoc-Python-2.tar.gz WiRoc-Python-2-0.35
-mv WiRoc-Python-2-0.35 WiRoc-Python-2
+tar xvfz WiRoc-Python-2.tar.gz WiRoc-Python-2-0.36
+mv WiRoc-Python-2-0.36 WiRoc-Python-2
 
 echo "WiRoc-BLE"
 #read line
