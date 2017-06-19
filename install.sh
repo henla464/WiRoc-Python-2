9e38fcb2c4ebacccf054cd0cfb332447531c6fce
Henrik Larsson <laselase@gmail.com>
2017-06-19 21:28:01 +0200

    update ble and python-2 packages

--- install.sh
+++ install.sh
@@ -61,18 +61,18 @@ apt-get -y install bluetooth bluez libbluetooth-dev libudev-dev
 echo "WiRoc-Python-2"
 #read line
 #install Python-2
-wget -O WiRoc-Python-2.tar.gz https://github.com/henla464/WiRoc-Python-2/archive/v0.36.tar.gz
+wget -O WiRoc-Python-2.tar.gz https://github.com/henla464/WiRoc-Python-2/archive/v0.37.tar.gz
 rm -rf WiRoc-Python-2
-tar xvfz WiRoc-Python-2.tar.gz WiRoc-Python-2-0.36
-mv WiRoc-Python-2-0.36 WiRoc-Python-2
+tar xvfz WiRoc-Python-2.tar.gz WiRoc-Python-2-0.37
+mv WiRoc-Python-2-0.37 WiRoc-Python-2
 
 echo "WiRoc-BLE"
 #read line
 #install WiRoc-BLE
-wget -O WiRoc-BLE-Device.tar.gz https://github.com/henla464/WiRoc-BLE-Device/archive/v0.08.tar.gz
+wget -O WiRoc-BLE-Device.tar.gz https://github.com/henla464/WiRoc-BLE-Device/archive/v0.09.tar.gz
 rm -rf WiRoc-BLE-Device
-tar xvfz WiRoc-BLE-Device.tar.gz WiRoc-BLE-Device-0.08
-mv WiRoc-BLE-Device-0.08 WiRoc-BLE-Device
+tar xvfz WiRoc-BLE-Device.tar.gz WiRoc-BLE-Device-0.09
+mv WiRoc-BLE-Device-0.09 WiRoc-BLE-Device
 
 #npm install -g node-gyp
 echo "install bluetooth-hci-socket"
@@ -83,7 +83,10 @@ echo "install bleno"
 #read line
 #install bleno
 npm install bleno
-#cd /home/chip
+wget -O /home/chip/node_modules/bleno/lib/bleno.js https://raw.githubusercontent.com/sandeepmistry/bleno/2548cc375646717ae76259d8a780159f9834b361/lib/bleno.js
+wget -O /home/chip/node_modules/bleno/lib/hci-socket/acl-stream.js https://raw.githubusercontent.com/sandeepmistry/bleno/2548cc375646717ae76259d8a780159f9834b361/lib/hci-socket/acl-stream.js
+wget -O /home/chip/node_modules/bleno/lib/hci-socket/bindings.js https://raw.githubusercontent.com/sandeepmistry/bleno/2548cc375646717ae76259d8a780159f9834b361/lib/hci-socket/bindings.js
+wget -O /home/chip/node_modules/bleno/lib/hci-socket/hci.js https://raw.githubusercontent.com/sandeepmistry/bleno/2548cc375646717ae76259d8a780159f9834b361/lib/hci-socket/hci.js
 
 
 echo "install startup scripts"
