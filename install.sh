96fc11a65be7976efa5e0a74dca39a703161f28b
Henrik Larsson <laselase@gmail.com>
2025-04-12 18:37:20 +0200

    update install to use venv

--- install.sh
+++ install.sh
@@ -39,9 +39,13 @@ lsb_release --all
 # Get the variables from the root settings yaml if it exists
 SETTINGSFILE=/root/settings.yaml
 if test -f "$SETTINGSFILE"; then
+  echo "using /root/settings.yaml"
   eval $(parse_yaml "$SETTINGSFILE")
+  echo "WiRoc Python 2 version: $WiRocPythonVersion"
+  echo "WiRoc BLE API version: $WiRocBLEAPIVersion"
+  echo "WiRoc HW Version: $WiRocHWVersion"
 else
-  WiRocPythonVersion="0.279"
+  WiRocPythonVersion="0.281"
   echo "Which WiRocPython2Version? [$WiRocPythonVersion]"
   read wPOption
   if ! [[ -z "$wPOption" ]];
@@ -49,12 +53,12 @@ else
     WiRocPythonVersion=$wPOption
   fi
 
-  WiRocBLEVersion="0.14"
-  echo "Which WiRocBLEVersion? [$WiRocBLEVersion]"
+  WiRocBLEAPIVersion="0.14"
+  echo "Which WiRocBLEAPIVersion? [$WiRocBLEAPIVersion]"
   read wBLEOption
   if ! [[ -z "$wBLEOption" ]];
   then
-    WiRocBLEVersion=$wBLEOption
+    WiRocBLEAPIVersion=$wBLEOption
   fi
 
   echo "Which hardware is this runnig on:"
@@ -109,7 +113,7 @@ echo "Settings.yaml"
 cat << EOF > settings.yaml
 WiRocDeviceName: WiRoc Device
 WiRocPythonVersion: ${WiRocPythonVersion}
-WiRocBLEAPIVersion: ${WiRocBLEVersion}
+WiRocBLEAPIVersion: ${WiRocBLEAPIVersion}
 WiRocHWVersion: ${WiRocHWVersion}
 EOF
 
@@ -127,26 +131,28 @@ echo "sqlite3"
 apt-get -y install libsqlite3-dev
 
 echo "python/pip"
+# needed for install scripts
 apt-get -y install python3-pip
-apt-get -y install python3-setuptools
-apt-get -y install python3-dev
-apt-get -y install gpiod
+## venv ## apt-get -y install python3-setuptools
+## venv ## apt-get -y install python3-dev
+## venv ## apt-get -y install gpiod
 
 # This allows pip to do system wide installs. Probably should change to use virtual environments though
 python3 -m pip config set global.break-system-packages true
 
 # WiRoc-Python
-pip3 install gpiod
-pip3 install --ignore-installed flask
-pip3 install --ignore-installed flask-swagger-ui
-pip3 install pyserial
-pip3 install jsonpickle
-pip3 install pyyaml
-pip3 install pyudev
-pip3 install daemonize
-pip3 install smbus2
-pip3 install Adafruit-Blinka
-pip3 install adafruit-circuitpython-ssd1306
+## venv ## pip3 install gpiod
+## venv ## pip3 install --ignore-installed flask
+## venv ## pip3 install --ignore-installed flask-swagger-ui
+## venv ## pip3 install pyserial
+## venv ## pip3 install jsonpickle
+## venv ## pip3 install pyyaml
+## venv ## pip3 install pyudev
+## venv ## pip3 install daemonize
+## venv ## pip3 install smbus2
+## venv ## pip3 install Adafruit-Blinka
+## venv ## pip3 install adafruit-circuitpython-ssd1306
+
 #not actually used but it is loaded by gpio because it thinks chip is bb
 #sudo apt-get install build-essential python3-dev python3-pip -y
 #git clone https://github.com/adafruit/adafruit-beaglebone-io-python.git
@@ -157,6 +163,7 @@ pip3 install adafruit-circuitpython-ssd1306
 apt-get -y install libgirepository1.0-dev
 apt-get -y install libcairo2-dev 
 apt-get -y install pkg-config
+apt-get -y install python3.12-venv
 pip3 install pycairo
 python3 -m pip install --ignore-installed PyGObject
 
@@ -164,15 +171,19 @@ apt-get -y install libtiff5-dev libjpeg-dev zlib1g-dev
 apt-get -y install python3-dev build-essential libssl-dev libffi-dev libxml2-dev libxslt1-dev zlib1g-dev
 apt-get -y install libfreetype6-dev
 apt-get -y install python3-numpy
-pip3 install pillow
+## venv ## pip3 install pillow
 
-pip3 install -U setuptools
-pip3 install wheel
+## venv ## pip3 install -U setuptools
+## venv ## pip3 install wheel
+
+## venv ## pip3 install cachetools
+
+# used in install scripts
 pip3 install requests
-pip3 install cachetools
+
+
+# reedsolomon
 pip3 install cython
-
-
 pip3 install build
 git clone https://github.com/tomerfiliba-org/reedsolomon.git
 cd reedsolomon
@@ -183,17 +194,15 @@ export DEB_PYTHON_INSTALL_LAYOUT=deb_system
 pip3 install dist/reedsolo-2.1.2b1-cp312-cp312-linux_armv7l.whl
 cd /home/chip
 
-
 #pip install reedsolo --pre
 #pip3 install --upgrade reedsolo --no-binary "reedsolo" --no-cache --config-setting="--build-option=--cythonize" --use-pep517 --isolated --verbose
 #pip3 install git+https://github.com/henla464/reedsolomon.git
 
 
 # WiRoc-BLE-API
-pip3 install dbus-python
+## venv ## pip3 install dbus-python
 ln -s /usr/lib/python3/dist-packages/_dbus_bindings.cpython-312-arm-linux-gnueabihf.so /usr/lib/python3/dist-packages/_dbus_bindings.so
 ln -s /usr/lib/python3/dist-packages/_dbus_glib_bindings.cpython-312-arm-linux-gnueabihf.so /usr/lib/python3/dist-packages/_dbus_glib_bindings.so
-
 
 
 
@@ -204,48 +213,95 @@ echo "Install bluetooth stuff"
 # bluez=5.50-1.2~deb10u2
 apt-get -y install bluetooth bluez libbluetooth-dev libudev-dev
 
+##################################
 echo "WiRoc-BLE"
+###################################
 
-#install WiRoc-BLE
-wget -O WiRoc-BLE-API.tar.gz https://github.com/henla464/WiRoc-BLE-API/archive/v$WiRocBLEVersion.tar.gz
+wget -O WiRoc-BLE-API.tar.gz https://github.com/henla464/WiRoc-BLE-API/archive/v$WiRocBLEAPIVersion.tar.gz
 rm -rf WiRoc-BLE-API
-tar xvfz WiRoc-BLE-API.tar.gz WiRoc-BLE-API-$WiRocBLEVersion
-mv WiRoc-BLE-API-$WiRocBLEVersion WiRoc-BLE-API
+tar xvfz WiRoc-BLE-API.tar.gz WiRoc-BLE-API-$WiRocBLEAPIVersion
+mv WiRoc-BLE-API-$WiRocBLEAPIVersion WiRoc-BLE-API
 mv WiRoc-BLE-API/installWiRocBLEAPI.sh .
 chmod ugo+x installWiRocBLEAPI.sh
 echo "Update WiRocBLEAPI version"
 
+apt-get -y install libdbus-1-dev
+cd WiRoc-BLE-API
+python3 -m venv env
+source env/bin/activate
+wget -O requirements.txt https://raw.githubusercontent.com/henla464/WiRoc-BLE-API/master/requirements.txt
+env/bin/pip install -r requirements.txt
+deactivate
+cd /home/chip
+
+wget -O /etc/systemd/system/WiRocBLEAPI.service https://raw.githubusercontent.com/henla464/WiRoc-StartupScripts/master/WiRocBLEAPI.service
+systemctl enable /etc/systemd/system/WiRocBLEAPI.service
+
+
+##################################
 echo "WiRoc-Python-2"
+###################################
+
 wget -O installWiRocPython.py https://raw.githubusercontent.com/henla464/WiRoc-Python-2/master/installWiRocPython.py
 chmod ugo+x installWiRocPython.py
 ./installWiRocPython.py $WiRocPythonVersion
 
+cd WiRoc-Python-2
+python3 -m venv env
+source env/bin/activate
+env/bin/pip install -r requirements.txt
+sudo env/bin/pip install ../reedsolomon/dist/reedsolo-2.1.2b1-cp312-cp312-linux_armv7l.whl
+deactivate
+cd /home/chip
 
+wget -O /etc/systemd/system/WiRocPython.service https://raw.githubusercontent.com/henla464/WiRoc-StartupScripts/master/WiRocPython.service
+wget -O /etc/systemd/system/WiRocPythonWS.service https://raw.githubusercontent.com/henla464/WiRoc-StartupScripts/master/WiRocPythonWS.service
+systemctl enable /etc/systemd/system/WiRocPython.service
+systemctl enable /etc/systemd/system/WiRocPythonWS.service
+
+###################################
 echo "install startup scripts"
+###################################
+
 mkdir WiRoc-StartupScripts
 wget -O /home/chip/WiRoc-StartupScripts/Startup.py https://raw.githubusercontent.com/henla464/WiRoc-StartupScripts/master/Startup.py
+wget -O /home/chip/WiRoc-StartupScripts/requirements.txt https://raw.githubusercontent.com/henla464/WiRoc-StartupScripts/master/requirements.txt
 chmod +x /home/chip/WiRoc-StartupScripts/Startup.py
+
+cd WiRoc-StartupScripts
+python3 -m venv env
+source env/bin/activate
+env/bin/pip install -r requirements.txt
+deactivate
+cd /home/chip
 
 echo "nanopiair"
 wget -O /usr/bin/devmem2 https://raw.githubusercontent.com/henla464/WiRoc-StartupScripts/master/devmem2
 chmod ugo+x /usr/bin/devmem2
-wget -O /etc/systemd/system/WiRocBLEAPI.service https://raw.githubusercontent.com/henla464/WiRoc-StartupScripts/master/WiRocBLEAPI.service
-systemctl enable /etc/systemd/system/WiRocBLEAPI.service
 
-wget -O /etc/systemd/system/WiRocPython.service https://raw.githubusercontent.com/henla464/WiRoc-StartupScripts/master/WiRocPython.service
-wget -O /etc/systemd/system/WiRocPythonWS.service https://raw.githubusercontent.com/henla464/WiRoc-StartupScripts/master/WiRocPythonWS.service
 wget -O /etc/systemd/system/WiRocStartup.service https://raw.githubusercontent.com/henla464/WiRoc-StartupScripts/master/WiRocStartup.service
 systemctl enable /etc/systemd/system/WiRocStartup.service
-systemctl enable /etc/systemd/system/WiRocPython.service
-systemctl enable /etc/systemd/system/WiRocPythonWS.service
 
-
+###################################
 echo "install wiroc-watchdog"
+###################################
+
 mkdir WiRoc-WatchDog
 wget -O /home/chip/WiRoc-WatchDog/WiRoc-WatchDog.py https://raw.githubusercontent.com/henla464/WiRoc-WatchDog/master/WiRoc-WatchDog.py
+wget -O /home/chip/WiRoc-WatchDog/requirements.txt https://raw.githubusercontent.com/henla464/WiRoc-WatchDog/master/requirements.txt
 chmod +x /home/chip/WiRoc-WatchDog/WiRoc-WatchDog.py
+
+cd WiRoc-WatchDog
+python3 -m venv env
+source env/bin/activate
+env/bin/pip install -r requirements.txt
+deactivate
+cd /home/chip
+
 wget -O /etc/systemd/system/WiRocWatchDog.service https://raw.githubusercontent.com/henla464/WiRoc-WatchDog/master/WiRocWatchDog.service
 systemctl enable /etc/systemd/system/WiRocWatchDog.service
+
+###################################
 
 echo "add user to dialout"
 sudo usermod -a -G dialout $USER
@@ -253,7 +309,7 @@ sudo usermod -a -G dialout $USER
 
 
 
-echo "nanopiair"
+
 if ! grep -Fxq "setenv video-mode sunxi:1920x1080,monitor=none,hpd=0,edid=1" /boot/boot.cmd
 then
     sed -i '$a setenv video-mode sunxi:1920x1080,monitor=none,hpd=0,edid=1' /boot/boot.cmd
